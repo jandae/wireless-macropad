ff1ff5500f2ff2868d1ed2e88a6c0a167b7dd1be
thejandae <tjandae@gmail.com>
2022-04-16 17:43:17 +0800

    clean up old code/comments

--- src/ble_keyboard.cpp
+++ src/ble_keyboard.cpp
@@ -11,8 +11,8 @@
         USAGE_PAGE(1),      0x07,          //   USAGE_PAGE (Kbrd/Keypad)
         USAGE_MINIMUM(1),   0x00,          //   USAGE_MINIMUM (0)
         USAGE_MAXIMUM(1),   0x73,          //   USAGE_MAXIMUM (0x65)
-    2. Unpair first when changes are made
-    3. Hold boot button when uploading to esp32
+    2. Unpair first when changes are made (true for non-beta)
+    3. Hold boot button when uploading to esp32 (rewired to a switch for ease)
     4. Using pin 12 causes it to not flash (remove first then flash)
 
  */
@@ -79,7 +79,7 @@ int rightState = 0;
 #define ROTARY_ENCODER_E_PIN 32
 #define ROTARY_ENCODER_F_PIN 35
 #define ROTARY_ENCODER_BUTTON_PIN 14
-#define ROTARY_ENCODER_BUTTON_PIN2 5 //5 // 5
+#define ROTARY_ENCODER_BUTTON_PIN2 5
 #define ROTARY_ENCODER_BUTTON_PIN3 1
 #define ROTARY_ENCODER_VCC_PIN -1
 
@@ -238,7 +238,7 @@ void resetKey(int row, int col){ //resetting the variables after key is released
   keyLong[row][col] = false;
   sendKey(layout[row][col], false); 
 }
-
+// first
 void rotary_onButtonClick()
 {
 	static unsigned long lastTimePressed = 0;
@@ -248,14 +248,10 @@ void rotary_onButtonClick()
 		return;
 	}
 
-  // bleKeyboard.write(KEY_F20);
   bleKeyboard.write(KEY_MEDIA_WWW_BACK);
-	// lastTimePressed = millis();
-	// Serial.print("button pressed ");
-	// Serial.print(millis());
-	// Serial.println(" milliseconds after restart");
 }
 
+// third
 void rotary_onButtonClick2()
 {
 	static unsigned long lastTimePressed = 0;
@@ -265,10 +261,10 @@ void rotary_onButtonClick2()
 		return;
 	}
 
-  // bleKeyboard.write(KEY_F23);
   bleKeyboard.write(KEY_MEDIA_WWW_STOP);
 }
 
+// middle
 void rotary_onButtonClick3()
 {
 	static unsigned long lastTimePressed = 0;
@@ -278,8 +274,6 @@ void rotary_onButtonClick3()
 		return;
 	}
 
-  // bleKeyboard.write(KEY_F23);
-  // bleKeyboard.write(KEY_F8);
   bleKeyboard.press(KEY_LEFT_CTRL);
   bleKeyboard.press(KEY_LEFT_ALT);
   bleKeyboard.press(KEY_F6);
@@ -304,6 +298,8 @@ void rotary_loop()
       Serial.println("==================");
       mode = 1;
       pressed = 0;
+      Serial.print("mode ");
+      Serial.println(mode);
     }
   }
 
@@ -463,14 +459,6 @@ void setup() {
   pinMode(toggl, INPUT_PULLDOWN);
   pinMode(toggl2, INPUT_PULLDOWN);
   pinMode(cup, INPUT_PULLDOWN);
-  // pinMode(btn1, INPUT_PULLDOWN);
-  // pinMode(btn2, INPUT_PULLDOWN);
-  // pinMode(btn4, INPUT_PULLDOWN);
-  // pinMode(btn5, INPUT_PULLDOWN);
-  // pinMode(btn6, INPUT_PULLDOWN);
-  // pinMode(btn7, INPUT_PULLDOWN);
-  // pinMode(btn8, INPUT_PULLDOWN);
-  // pinMode(btn9, INPUT_PULLDOWN);
   pinMode(left, INPUT_PULLUP);
   pinMode(right, INPUT_PULLUP);
 
@@ -504,37 +492,18 @@ void loop() {
     togglState = digitalRead(toggl);
     toggl2State = digitalRead(toggl2);
     cupState = digitalRead(cup);
-    // btn1State = digitalRead(btn1);
-    // btn2State = digitalRead(btn2);
-    // btn4State = digitalRead(btn4);
-    // btn5State = digitalRead(btn5);
-    // btn6State = digitalRead(btn6);
-    // btn7State = digitalRead(btn7);
-    // btn8State = digitalRead(btn8);
-    // btn9State = digitalRead(btn9);
     leftState = digitalRead(left);
     rightState = digitalRead(right);
-
-    // Serial.print(leftState);
-    // Serial.println(rightState);
-
     
 
     rotary_loop();
-    // Serial.println(currentStateCLK);
-    // Serial.print(togglState);[]]
-    // Serial.print('-');
-    // Serial.print(btn1State);
-    // Serial.println('+');
 
     if ( (millis()-oldtime) > 100) {
-      if (togglState == 1) {
-        // bleKeyboard.press(KEY_F13);
+      if (togglState == 1) {        
         bleKeyboard.press('f');
         Serial.println("f");
       } else if (togglState == 0) {
-        bleKeyboard.release('f');
-        // bleKeyboard.release(KEY_F13);
+        bleKeyboard.release('f');        
       }
 
       if (toggl2State == 1) {
@@ -544,74 +513,14 @@ void loop() {
         bleKeyboard.release('g');
       }
 
-      if (cupState == 1) {
-        // bleKeyboard.press(KEY_F16);
-        // Serial.println("F16");
+      if (cupState == 1) {        
         cupaaa = 1;
       } else if (cupState == 0) {
         if (cupaaa == 1) {
           bleKeyboard.write(KEY_F16);
           cupaaa = 0;
         }
-        // bleKeyboard.release(KEY_F16);
-      }
-
-      // if (btn1State == 1) {
-      //   bleKeyboard.press(KEY_F14);
-      //   Serial.println("F14");
-      // } else if (btn1State == 0) {
-      //   bleKeyboard.release(KEY_F14);
-      // }
-
-      // if (btn2State == 1) {
-      //   bleKeyboard.press(KEY_F15);
-      //   Serial.println("F15");
-      // } else if (btn2State == 0) {
-      //   bleKeyboard.release(KEY_F15);
-      // }
-
-      // if (btn4State == 1) {
-      //   bleKeyboard.press(KEY_F17);
-      //   Serial.println("F17");
-      // } else if (btn4State == 0) {
-      //   bleKeyboard.release(KEY_F17);
-      // }
-
-      // if (btn5State == 1) {
-      //   bleKeyboard.press(KEY_F24);
-      //   Serial.println("F24");
-      // } else if (btn5State == 0) {
-      //   bleKeyboard.release(KEY_F24);
-      // }
-
-      // if (btn6State == 1) {
-      //   bleKeyboard.press(KEY_F20);        
-      //   Serial.println("f20");
-      // } else if (btn6State == 0) {        
-      //   bleKeyboard.release(KEY_F20);
-      // }
-     
-
-      // if (btn7State == 1) {
-      //   bleKeyboard.press(KEY_F23);
-      //   Serial.println("F23");
-      // } else if (btn7State == 0) {
-      //   bleKeyboard.release(KEY_F23);
-      // }
-
-      // if (btn8State == 1) {
-      //   bleKeyboard.press(KEY_F13);
-      //   Serial.println("f13");
-      // } else if (btn8State == 0) {
-      //   bleKeyboard.release(KEY_F13);
-      // }
-
-      // if (btn9State == 1) {
-      //   bleKeyboard.press(KEY_MEDIA_WWW_BOOKMARKS);
-      //   Serial.println("KEY_MEDIA_WWW_BOOKMARKS");
-      // } else if (btn9State == 0) {
-      //   bleKeyboard.release(KEY_MEDIA_WWW_BOOKMARKS);
-      // }
+      }      
 
       if (leftState == 0) {
         bleKeyboard.press('[');
@@ -652,35 +561,5 @@ void loop() {
       delayMicroseconds(500);         //setting the row high and waiting 0.5ms until next cycle
     }
 
-//    Serial.println("Sending 'Hello world'...");
-//    bleKeyboard.print("Hello world");
-//
-//    delay(1000);
-//
-//    Serial.println("Sending Enter key...");
-//    bleKeyboard.write('z');
-//
-//    delay(1000);
-//
-//    Serial.println("Sending Play/Pause media key...");
-//    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-//
-//    delay(1000);
-
-
-   //
-   // Below is an example of pressing multiple keyboard modifiers
-   // which by default is commented out.
-   //
-   /* Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(100);
-    bleKeyboard.releaseAll();
-    */
-
   }
-//  Serial.println("Waiting 5 seconds...");
-//  delay(5000);
 }
